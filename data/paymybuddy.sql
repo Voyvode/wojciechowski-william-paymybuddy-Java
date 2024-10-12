64a43248d8111671701cccc18e3a6d53bdfbb62e
Voyvode <voyvode@yandex.com>
2024-10-12 18:52:39 +0200

    Update database

--- data/paymybuddy.sql
+++ data/paymybuddy.sql
@@ -2,21 +2,24 @@ CREATE TABLE customer (
     id SERIAL PRIMARY KEY,
     username VARCHAR(32) UNIQUE NOT NULL,
     email VARCHAR(254) UNIQUE NOT NULL,
-    password_hash CHAR(40)
+    password_hash CHAR(60), -- bcrypt
+	signup TIMESTAMP NOT NULL
 );
+CREATE INDEX index_username ON customer(username); -- user search optimization
 
 CREATE TABLE buddy (
 	PRIMARY KEY (customer_id, buddy_id),
-	customer_id INT REFERENCES customer(id),
-	buddy_id INT REFERENCES customer(id),
+	customer_id INT REFERENCES customer(id) NOT NULL,
+	buddy_id INT REFERENCES customer(id) NOT NULL,
 	CONSTRAINT no_self_buddy CHECK (customer_id != buddy_id),
 	CONSTRAINT unique_buddy UNIQUE (customer_id, buddy_id)
 );
 
 CREATE TABLE transfer (
     id SERIAL PRIMARY KEY,
-    sender INT REFERENCES customer(id),
-    receiver INT REFERENCES customer(id),
+    sender INT REFERENCES customer(id) NOT NULL,
+    receiver INT REFERENCES customer(id) NOT NULL,
     description TEXT,
-    amount NUMERIC(7,5)
+    amount NUMERIC(4,2) CHECK (amount > 0 AND amount <= 1000),
+	timestamp TIMESTAMP NOT NULL
 );
