f729bd8f60b63b23b3867252b90417324030d2e0
Voyvode <voyvode@yandex.com>
2024-10-12 18:53:19 +0200

    Add Application

---
+++ src/main/java/com/paymybuddy/Application.java
@@ -0,0 +1,21 @@
+package com.paymybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application class for Pay My Buddy.
+ */
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * Entry point for the application.
+	 *
+	 * @param args Command line arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
