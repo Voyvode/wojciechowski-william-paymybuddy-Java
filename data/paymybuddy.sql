d3059a9ba46b8a8ae02a6b7171005797dde8840e
Voyvode <voyvode@yandex.com>
2024-11-28 03:08:57 +0100

    Update SQL schema

--- data/paymybuddy.sql
+++ data/paymybuddy.sql
@@ -3,28 +3,25 @@ CREATE TABLE customer
 	id            SERIAL PRIMARY KEY,
 	username      VARCHAR(32) UNIQUE  NOT NULL,
 	email         VARCHAR(254) UNIQUE NOT NULL,
-	password_hash CHAR(60)            NOT NULL, -- bcrypt
+	password_hash CHAR(60)            NOT NULL,
 	created_at    TIMESTAMP           NOT NULL,
 	last_login_at TIMESTAMP           NOT NULL
 );
-CREATE INDEX index_username ON customer (username); -- user search optimization
-CREATE INDEX index_email ON customer (email); -- user search optimization
 
 CREATE TABLE customer_buddy
 (
+	customer_id INTEGER NOT NULL REFERENCES customer (id),
+	buddy_id    INTEGER NOT NULL REFERENCES customer (id),
 	PRIMARY KEY (customer_id, buddy_id),
-	customer_id INTEGER REFERENCES customer (id) NOT NULL,
-	buddy_id    INTEGER REFERENCES customer (id) NOT NULL,
-	CONSTRAINT no_self_buddy CHECK (customer_id != buddy_id),
-	CONSTRAINT unique_buddy UNIQUE (customer_id, buddy_id)
+	CONSTRAINT check_no_self_buddy CHECK (customer_id != buddy_id)
 );
 
 CREATE TABLE transfer
 (
 	id          SERIAL PRIMARY KEY,
-	sender      INTEGER REFERENCES customer (id) NOT NULL,
-	receiver    INTEGER REFERENCES customer (id) NOT NULL,
+	sender      INTEGER   NOT NULL REFERENCES customer (id),
+	receiver    INTEGER   NOT NULL REFERENCES customer (id),
 	description VARCHAR(255),
-	amount      NUMERIC(5, 2) CHECK (amount > 0 AND amount <= 1000),
-	timestamp   TIMESTAMP                    NOT NULL
+	amount      NUMERIC(6, 2) CHECK (amount > 0 AND amount <= 1000),
+	timestamp   TIMESTAMP NOT NULL
 );
